4963b171ac98cfedf096d079bc5c26f0a1a0512a
RalphEgg <theslugprince@gmail.com>
2023-04-24 17:56:47 -0700

    Lab 4/Link Overhaul

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,14 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Davis Goodman
+// Date: 4-24-2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Print game instructions
+  // Accept user input
+  // Validate user input
+// Choose computer move
+  // Test win conditions
+// Print result
+// Print game options [Would you like to play again? y/n]
+  // Accept user input
+  // Validate user input
+// Repeat
