1d731a55e9e709c2c070df22f5bb696774d05f62
RalphEgg <theslugprince@gmail.com>
2023-05-08 19:47:17 -0700

    Lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 8 js file
+// Author: Davis Goodman 
+// Date: 5/8/2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function double(x){
+  return (x * 2);
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Test Function
+console.log("What is 2x2? ", double(2))
+console.log("What is 10x2? ", double(10))
 
-// let's get this party started
-main();
+// Array
+numbers = [3, 5, 12, 30, 50]
+console.log("My Array: ", numbers);
+
+// Function Results
+var result = numbers.map(double)
+console.log("Doubled Array: ", result);
+
+var result = numbers.map(function(x){
+  return x * (3/4);
+})
+console.log("3/4ths Array: ", result);
