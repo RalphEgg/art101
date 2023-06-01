e416b9716ea68462c3f458e87d2b95e61ffab3ce
RalphEgg <theslugprince@gmail.com>
2023-05-31 21:54:20 -0700

    Lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,5 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 15 JS
+// Author: Davis Goodman
+// Date: 5/31/2023
 
-// 
+// Random Pokemon API
+
+function getStuff(){
+    console.log("Click");
+    $.ajax({
+        url: "https://pokeapi.co/api/v2/pokemon/" + Math.floor((Math.random() * 1008)) + "/",
+        type: "GET",
+        data: {},
+    })
+    .done(function(data){
+        // console.log(data);
+        $("#output").html("It's <span>" + data.name + "</span>!");
+        console.log(data.name + ": " + data.id);
+    })
+}
+
+$("button").click(getStuff);
